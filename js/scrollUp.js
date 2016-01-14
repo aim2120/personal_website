facf9f8596430bcec2876237b0bd0b70a4f09839
Annalise Mariottini <annaliseirene@msn.com>
2016-01-14 01:15:38 -0800

    improved top link accuracy

--- js/scrollUp.js
+++ js/scrollUp.js
@@ -1,9 +1,15 @@
-var timeOut;
-var main = document.getElementById("scrolling").getBoundingClientRect();
+var timeOut, topOfPage;
+var mq = window.matchMedia("(max-device-width:769px)");
+if(mq.matches) {
+	topOfPage = 0;
+} else {
+	topOfPage = document.getElementById("scrolling").offsetTop;
+}
+
 function scrollToTop() {
-	if (document.body.scrollTop >= main.top || document.documentElement.scrollTop >= main.top){
+	if (document.body.scrollTop > topOfPage || document.documentElement.scrollTop > topOfPage){
 		window.scrollBy(0,-50);
-		timeOut=setTimeout('scrollToTop()',5);
+		timeOut=setTimeout('scrollToTop()',10);
 	} else {
 		clearTimeout(timeOut);
 	}
