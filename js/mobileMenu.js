6add23e26354566742b5e1e54b335925a018501f
Annalise Mariottini <annaliseirene@msn.com>
2020-07-10 21:46:40 -0400

    mobile menu button

---
+++ js/mobileMenu.js
@@ -0,0 +1,18 @@
+function mobileMenuInit() {
+    var button = document.getElementById("navbutton");
+    button.style.display = "block";
+    var menu = document.getElementsByTagName("nav")[0];
+    menu.style.display = "none";
+};
+
+function mobileMenuToggle() {
+    var button = document.getElementById("navbutton");
+    var menu = document.getElementsByTagName("nav")[0];
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+        button.className = "inverse";
+    } else {
+        menu.style.display = "none";
+        button.className = "";
+    }
+}
